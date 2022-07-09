3e2fa43eeb0bb688218a76f925be2d34ca07236c
Sane905 <konjaku0905@yahoo.co.jp>
2022-07-09 16:54:13 +0900

    feat:prettier追加

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,21 +1,21 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../../styles/Home.module.css";
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+
+import styles from "../../styles/Home.module.css"
 
 const Home: NextPage = () => {
+  type Avenger = {
+    name: string
+    email: string
+  }
 
-type Avenger = {
-  name:string
-  email:string
-}
+  const a: Avenger = {
+    name: "w",
+    email: "d",
+  }
 
-const a: Avenger = {
-  name:"w",
-  email:"d"
-}
-
-console.log(a)
+  console.log(a)
 
   return (
     <div className={styles.container}>
@@ -31,8 +31,7 @@ console.log(a)
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
@@ -46,10 +45,7 @@ console.log(a)
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -59,9 +55,7 @@ console.log(a)
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -79,7 +73,7 @@ console.log(a)
         </a>
       </footer>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
